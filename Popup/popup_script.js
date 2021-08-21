c97fa1e12529a2a576b38164253171fef4dfe723
Willie Pai <PaisWillie@gmail.com>
2021-08-21 15:25:39 -0400

    Spotify Test

--- Popup/popup_script.js
+++ Popup/popup_script.js
@@ -1,7 +1,8 @@
-ce_button = document.getElementById("get-playlists");
+ce_get_playlists_button = document.getElementById("get-playlists");
+ce_sign_in_button = document.getElementById("sign-in");
 
-if (ce_button) {
-    ce_button.addEventListener("click", () => {
+if (ce_get_playlists_button) {
+    ce_get_playlists_button.addEventListener("click", () => {
         chrome.runtime.sendMessage(
             {
                 message: "fetch_playlists",
@@ -9,7 +10,23 @@ if (ce_button) {
             },
             (response) => {
                 if (response.message === "success") {
-                    ce_button.innerHTML = `Working!`;
+                    ce_get_playlists_button.innerHTML = `Working!`;
+                }
+            }
+        );
+    });
+}
+
+if (ce_sign_in_button) {
+    ce_sign_in_button.addEventListener("click", () => {
+        chrome.runtime.sendMessage(
+            {
+                message: "login",
+                payload: null,
+            },
+            (response) => {
+                if (response.message === "success") {
+                    ce_sign_in_button.innerHTML = `Working!`;
                 }
             }
         );
--- background.js
+++ background.js
@@ -1,33 +1,60 @@
-chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.local.set({
-        name: "Willie",
+// Runs on initial startup
+// chrome.runtime.onInstalled.addListener(() => {
+//     chrome.storage.local.set({
+//         name: "Willie",
+//     });
+// });
+
+// chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+//     // Checks if page loading is complete and URL starts with http
+//     if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
+//         chrome.scripting
+//             .insertCSS({
+//                 target: { tabId: tabId },
+//                 files: ["./Foreground/foreground_styles.css"],
+//             })
+//             .then(() => {
+//                 console.log("Injected foreground styles.");
+
+//                 chrome.scripting
+//                     .executeScript({
+//                         target: { tabId: tabId },
+//                         files: ["./Foreground/foreground.js"],
+//                     })
+
+//                     .then(() => {
+//                         console.log("Injected the foreground script.");
+//                     });
+//             })
+//             .catch((err) => console.log(err));
+//     }
+// });
+
+// var CONFIG = require('./config.json');
+
+// TODO: Censor variables
+const CLIENT_ID = encodeURIComponent("39d98902d36a47ed9bd7a6c50e571a20");
+const CLIENT_SECRET = encodeURIComponent("d4347ad265fb47c9b6acb80ca1ab7a3f");
+const REDIRECT_URI = encodeURIComponent("http://localhost:8888/callback");
+const STATE = encodeURIComponent(
+    "meet" + Math.random().toString(36).substring(2, 15)
+);
+const SCOPE = encodeURIComponent("user-read-email");
+const SHOW_DIALOG = encodeURIComponent("true");
+
+const getToken = async () => {
+    const result = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            Authorization: "Basic " + btoa(CLIENT_ID + ":" + CLIENT_SECRET),
+        },
+        body: "grant_type=client_credentials",
     });
-});
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    // Checks if page loading is complete and URL starts with http
-    if (changeInfo.status === "complete" && /^http/.test(tab.url)) {
-        chrome.scripting
-            .insertCSS({
-                target: { tabId: tabId },
-                files: ["./Foreground/foreground_styles.css"],
-            })
-            .then(() => {
-                console.log("Injected foreground styles.");
-
-                chrome.scripting
-                    .executeScript({
-                        target: { tabId: tabId },
-                        files: ["./Foreground/foreground.js"],
-                    })
-
-                    .then(() => {
-                        console.log("Injected the foreground script.");
-                    });
-            })
-            .catch((err) => console.log(err));
-    }
-});
+    const data = await result.json();
+    return data.access_token;
+};
 
 const fetchPlaylists = async (token) => {
     const result = await fetch(`https://api.spotify.com/v1/me/playlists`, {
@@ -36,31 +63,75 @@ const fetchPlaylists = async (token) => {
     });
 
     const data = await result.json();
-    return data.categories.items;
+    return data;
 };
 
+let user_signed_in = false;
+
+function create_spotify_endpoint() {
+    let oauth2_url = `https://accounts.spotify.com/authorize
+    ?client_id=${CLIENT_ID}
+    &response_types=${REPONSE_TYPE}
+    &redirect_uri=${REDIRECT_URI}
+    &state=${STATE}
+    &scope=${SCOPES}
+    &show_dialog=${SHOW_DIALOG}
+    `;
+
+    console.log(oauth2_url);
+
+    return oauth2_url;
+}
+
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.message === "fetch_playlists") {
-        // chrome.storage.local.get("name", (data) => {
-        //     if (chrome.runtime.lastError) {
-        //         sendResponse({
-        //             message: "failed",
-        //         });
-        //         return;
-        //     }
+    if (request.message === "login") {
+        if (user_signed_in) {
+            console.log("User is already signed in.");
 
-        //     sendResponse({
-        //         message: "success",
-        //         payload: data.name,
-        //     });
-        // });
+            return true;
+        } else {
+            chrome.identity.launchWebAuthFlow(
+                {
+                    url: create_spotify_endpoint(),
+                    interactive: true,
+                },
+                function (redirect_url) {
+                    if (chrome.runtime.lastError) {
+                        sendResponse({
+                            message: "failed",
+                        });
+                    } else {
+                        if (
+                            redirect_url.includes(
+                                "callback?error=access_denied"
+                            )
+                        ) {
+                            sendResponse({
+                                message: "failed",
+                            });
+                        } else {
+                            console.log(redirect_url);
+                        }
+                    }
+                }
+            );
+        }
 
-        console.log(fetchPlaylists());
+        return true;
+
+    } else if (request.message === "logout") {
+        user_signed_in = false;
+        
+        return true;
+
+    } else if (request.message === "fetch_playlists") {
+        console.log(fetchPlaylists(getToken()));
         sendResponse({
             message: "success",
-            payload: null,
         });
 
         return true;
     }
+
+    return true;
 });
